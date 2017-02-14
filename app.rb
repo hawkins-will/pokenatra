c4fae7402eaadd5a3484d53c0baeb8fe942e5f38
Will Hawkins <willhawkins123@yahoo.com>
2017-02-14 16:25:27 -0500

    Added Edit and Delete Pages

--- app.rb
+++ app.rb
@@ -21,6 +21,17 @@ get "/pokemon/new" do
   erb :"new"
 end
 
+get "/pokemon/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"/edit"
+end
+
+put "/pokemon/:id" do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemon/#{@pokemon.id}")
+end
+
 post "/pokemon" do
   @pokemon = Pokemon.create(params[:pokemon])
   redirect "/pokemon/#{@pokemon.id}"
@@ -29,4 +40,10 @@ end
 get "/pokemon/:id" do
   @pokemon = Pokemon.find(params[:id])
   erb :pokemon
+end
+
+delete "/pokemon/:id" do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect("/")
 end
